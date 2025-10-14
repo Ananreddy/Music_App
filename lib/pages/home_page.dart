517da0213e093cc177a6a927ee01f2dcb3471ccf
Anan_reddy <ananreddy890@gmail.com>
2025-10-14 12:11:54 +0530

    Implemented Homepage UI with PlaylistProvider and song navigation to SongPage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,37 +2,69 @@ import 'package:flutter/material.dart';
 import 'package:music_app/components/drawer.dart';
 import 'package:music_app/models/playlist_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:music_app/models/song.dart';
 
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
 
- 
- class Homepage extends StatefulWidget{
-         const Homepage({super.key});
-          
-       @override
-       State<Homepage> createState() => _HomePageState();
-         }
+  @override
+  State<Homepage> createState() => _HomePageState();
+}
 
- class _HomePageState extends State<Homepage> {
-      @override
-      Widget build(BuildContext context) {
-        return Scaffold(
-          backgroundColor: Theme.of(context).colorScheme.surface,
-          appBar:AppBar(
-          title:Text("P L A Y L I S T"),
-          centerTitle:true,
+class _HomePageState extends State<Homepage> {
+  ///get the playlist provider
+  late final dynamic playlistProvider;
 
-              
-        ),
+  @override
+  void initState() {
+    super.initState();
+    //get playlist provider
+    playlistProvider = Provider.of<PlaylistProvider>(context, listen: false);
+  }
+    //goto a song
+    void goToSong(int songIndex) {
+      //update current song index
+      playlistProvider.currentSongIndex=songIndex;
 
-           body: Consumer<PlaylistProvider>(
-           builder: (context, value, child) =>  ListView.builder(
-           itemBuilder: (context,index)=>ListTile(),
-            
-            ),
-           ),   
-           
-          drawer: const MyDrawer(),
-          
-         );
-      }
-   }        
+     //navigate to song page
+     Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => SongPage(),),
+
+     ); 
+    }   
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: AppBar(title: Text("P L A Y L I S T"), centerTitle: true),
+
+      body: Consumer<PlaylistProvider>(
+        builder: (context, value, child) {
+          //get the playlist
+          final List<Song> playlist = value.playlist;
+
+          //return list view UI
+          return ListView.builder(
+            itemCount: playlist.length,
+            itemBuilder: (context, index) {
+              //get individual song
+              final Song song = playlist[index];
+
+              //return list tile
+              return ListTile(
+                title: Text(song.songName),
+                subtitle: Text(song.artistName),
+                leading: Image.asset(song.albumArtImagePath),
+                onTap: () => goToSong(index),
+              );
+            },
+          );
+        },
+      ),
+
+      drawer: const MyDrawer(),
+    );
+  }
+}
